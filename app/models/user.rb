357128c258c0741315ec7c2c4ea90228c203d004
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-11-20 16:47:40 +0900

    ユーザー登録フォームへ正規表現の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+         
+
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true
+  end
   validates :birth_date, presence: true
 
        
