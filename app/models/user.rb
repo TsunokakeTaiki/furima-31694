e52b0c68e784fb9a13ef06a13f58ff34ef503f93
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-11-24 18:22:03 +0900

    usersモデルの重複している記述presence: trueの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,12 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true, length: { maximum: 40 }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name, presence: true
-    validates :last_name, presence: true
+    validates :first_name
+    validates :last_name
   end
   with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カナを使用してください' } do
-    validates :first_name_kana, presence: true
-    validates :last_name_kana, presence: true
+    validates :first_name_kana
+    validates :last_name_kana
   end
   validates :birth_date, presence: true
 
