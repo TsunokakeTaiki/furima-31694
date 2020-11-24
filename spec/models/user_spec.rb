8a6493596d85349971d2d607320f41cb779fd1a2
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-11-24 18:26:17 +0900

    単体テストコードへ「emailに@がない場合は登録できない」を追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,6 +51,11 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it "emailに@がない場合は登録できない" do
+        @user.email = 'test.co.jp'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it "重複したemailが存在する場合登録できない" do
         @user.save
         another_user = FactoryBot.build(:user)
