76b5fcdf0fe2302cd444435bc7ed186e537bd88c
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-11-25 19:00:15 +0900

    categoryモデルのActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,8 @@ class Item < ApplicationRecord
     validates :price
   end
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  validates :category_id, numericality: { other_than: 1 } 
+
 end
