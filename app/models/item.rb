932729d003ca366b7a1ec3f6e5fc02d867fafc98
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-11-25 19:24:20 +0900

    prefectureモデルの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,11 +14,13 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :sales_status
   belongs_to :shipping_fee_status
+  belongs_to :prefecture
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :sales_status_id
     validates :shipping_fee_status_id
+    validates :prefecture_id
   end
 
 end
