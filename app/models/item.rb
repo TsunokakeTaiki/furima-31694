01d962748379001a53f01e3255f5b6269cc3b62e
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-11-26 18:56:23 +0900

    itemモデルのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,8 @@ class Item < ApplicationRecord
   has_one :purchase
   has_one_attached :image
 
-  with_options presence: true do
-    validates :name
-    validates :info
-    validates :price
-  end
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :info, presence: true, length: { maximum: 1000 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -24,5 +21,12 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :scheduled_delivery_id
   end
+
+  validates :price, presence: true,
+  :numericality => {
+    :greater_than_or_equal_to => 300,
+    :less_than_or_equal_to => 9999999,
+    :message => '¥300〜9,999,999の範囲で入力してください' 
+  }
 
 end
