a4189dedf51af18be53d56211121404d153012b6
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-11-26 19:35:40 +0900

    ログイン状態によるページ遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,16 @@
 class ItemsController < ApplicationController
 
+  before_action :authenticate_user!, except: [:index, :new, :show]
+  before_action :mismatch_login, only: [:edit, :update, :destroy]
+
   def index
   end
 
   def new
     @item = Item.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
   def create
@@ -20,5 +26,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price, ).merge(user_id: current_user.id)
+  end
+
+  def mismatch_login
+    unless current_user == Item.find(params[:id]).user
+      redirect_to root_path 
+    end
   end
 end
