0ef3107fdc8e925a5882536a83371eb1a83f3cff
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-12-01 22:34:49 +0900

    purchaseモデルのActiveModelの設定

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,14 @@
 class Purchase < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :token
+
   belongs_to :item
   belongs_to :user
   has_one :send_address
+
+  # ここにバリデーションの処理を書く
+
+  def save
+    # 各テーブルにデータを保存する処理を書く
+  end
 end
