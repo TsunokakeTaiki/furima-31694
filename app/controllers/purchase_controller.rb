d3f5044a8a962de551d1901046fc74a1be18d7ca
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-12-02 01:16:51 +0900

    purchaseコントローラーのアクション設定

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,2 +1,14 @@
 class PurchaseController < ApplicationController
+
+  def index
+  end
+
+  def create
+    Purchase.create(purchase_params)
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
