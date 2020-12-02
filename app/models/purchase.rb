be78b1608089851dd035ac20aa92ccd403d1e985
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-12-02 16:11:36 +0900

    Formオブジェクトの修正、Item_purchaseモデルの作成

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,14 +1,5 @@
 class Purchase < ApplicationRecord
-  include ActiveModel::Model
-  attr_accessor :token
-
   belongs_to :item
   belongs_to :user
   has_one :send_address
-
-  # ここにバリデーションの処理を書く
-
-  def save
-    # 各テーブルにデータを保存する処理を書く
-  end
 end
