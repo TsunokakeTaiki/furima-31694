b0d7bcb708d03bd99784a82b26589d2175bcaaa4
TsunokakeTAiki <tsuno.tai@gmail.com>
2020-12-03 15:32:17 +0900

    set_itemのbeforeアクションの設定

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,7 +1,8 @@
 class PurchaseController < ApplicationController
 
+  before_action :set_item
+
   def index
-    @item = Item.find(params[:item_id])
     @item_purchase = ItemPurchase.new
   end
 
@@ -12,12 +13,15 @@ class PurchaseController < ApplicationController
       @item_purchase.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render action: :index 
     end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def purchase_params
     params.require(:item_purchase).permit(:postial_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
